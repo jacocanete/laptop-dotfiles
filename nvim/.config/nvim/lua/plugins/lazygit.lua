4c5e9d3b0649de56a2f13e18440e98242e37a2b7
jacocanete <jacogagarincanete@gmail.com>
2026-02-01 15:51:13 +0800

    chore: add serena configuration and update dotfiles

--- nvim/.config/nvim/lua/plugins/lazygit.lua
+++ nvim/.config/nvim/lua/plugins/lazygit.lua
@@ -10,17 +10,9 @@ return {
     },
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
     },
-    config = function()
-      -- Load telescope extension for lazygit
-      require("telescope").load_extension("lazygit")
-    end,
     keys = {
-      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
-      { "<leader>gf", "<cmd>LazyGitCurrentFile<cr>", desc = "LazyGit Current File" },
-      { "<leader>gc", "<cmd>LazyGitConfig<cr>", desc = "LazyGit Config" },
-      { "<leader>gl", "<cmd>Telescope lazygit<cr>", desc = "Telescope LazyGit" },
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
     },
   },
 }
